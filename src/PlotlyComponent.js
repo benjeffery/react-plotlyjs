8cf5fd0719a498651fca2f9028568b29be2a9e5d
Yusaku Sako <yusaku@hortonworks.com>
2016-02-26 20:07:33 -0800

    Add event handling

--- src/PlotlyComponent.js
+++ src/PlotlyComponent.js
@@ -7,7 +7,12 @@ var PlotlyComponent = React.createClass({
   propTypes: {
     data: React.PropTypes.array,
     layout: React.PropTypes.object,
-    config: React.PropTypes.object
+    config: React.PropTypes.object,
+    onClick: React.PropTypes.func,
+    onBeforeHover: React.PropTypes.func,
+    onHover: React.PropTypes.func,
+    onUnHover: React.PropTypes.func,
+    onSelected: React.PropTypes.func
   },
 
   shouldComponentUpdate(nextProps) {
@@ -18,6 +23,11 @@ var PlotlyComponent = React.createClass({
   componentDidMount() {
     let {data, layout, config} = this.props;
     Plotly.plot(this.container, data, layout, config);
+    this.props.onClick && this.container.on('plotly_click', this.props.onClick);
+    this.props.onBeforeHover && this.container.on('plotly_beforehover', this.props.onBeforeHover);
+    this.props.onHover && this.container.on('plotly_hover', this.props.onHover);
+    this.props.onUnHover && this.container.on('plotly_unhover', this.props.onUnHover);
+    this.props.onSelected && this.container.on('plotly_selected', this.props.onSelected);
   },
 
   componentDidUpdate() {
@@ -25,7 +35,14 @@ var PlotlyComponent = React.createClass({
     this.container.data = this.props.data;
     this.container.layout = this.props.layout;
     Plotly.redraw(this.container);
+  },
 
+  componentWillUnmount: function() {
+    this.container.removeAllListeners('plotly_click');
+    this.container.removeAllListeners('plotly_beforehover');
+    this.container.removeAllListeners('plotly_hover');
+    this.container.removeAllListeners('plotly_unhover');
+    this.container.removeAllListeners('plotly_selected');
   },
 
   render: function () {
