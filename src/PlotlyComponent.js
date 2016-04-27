ba6c66903279dc26efe0bd44557c64d9cd1af812
Harry Jeffery <harry@exec64.co.uk>
2016-04-27 11:33:13 +0100

    Fix layout bugs caused by plotly cruft

--- src/PlotlyComponent.js
+++ src/PlotlyComponent.js
@@ -43,6 +43,10 @@ var PlotlyComponent = React.createClass({
   },
 
   componentWillUnmount: function() {
+    //Remove some cruft left behind by plotly
+    var cruft = document.getElementById("js-plotly-tester");
+    cruft.parentNode.removeChild(cruft);
+
     this.container.removeAllListeners('plotly_click');
     this.container.removeAllListeners('plotly_beforehover');
     this.container.removeAllListeners('plotly_hover');
